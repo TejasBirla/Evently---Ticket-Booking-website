0ab798a56cb5dbdbd7143f83df04bfa03b51054e
Tejas Birla <tejasbirla3@gmail.com>
2025-07-07 15:28:32 +0530

    Confirmation dialog used for deleting event. (Updated UI)

--- admin/src/Components/AdminPanel/DeleteEvent/DeleteEvent.jsx
+++ admin/src/Components/AdminPanel/DeleteEvent/DeleteEvent.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { EventContext } from "../../../Contexts/EventContext.jsx";
+import ConfirmDialog from "../ConfirmDialog/ConfirmDialog.jsx";
 import "./DeleteEvent.css";
 
 export default function DeleteEvent() {
   const { events, getAllEvents, deleteEvent } = useContext(EventContext);
   const [selectedEventId, setSelectedEventId] = useState("");
+  const [showDialog, setShowDialog] = useState(false);
 
   useEffect(() => {
     getAllEvents();
@@ -12,13 +14,16 @@ export default function DeleteEvent() {
 
   const handleDelete = () => {
     if (!selectedEventId) return;
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete this event?"
-    );
-    if (confirmDelete) {
-      deleteEvent(selectedEventId);
-      setSelectedEventId("");
-    }
+    setShowDialog(true);
+  };
+  const handleConfirm = () => {
+    deleteEvent(selectedEventId);
+    setSelectedEventId("");
+    setShowDialog(false);
+  };
+
+  const handleCancel = () => {
+    setShowDialog(false);
   };
 
   return (
@@ -44,6 +49,15 @@ export default function DeleteEvent() {
       >
         Delete Event
       </button>
+
+      {showDialog && (
+        <ConfirmDialog
+          title="Confirm Deletion"
+          message="Are you sure you want to delete this event?"
+          onConfirm={handleConfirm}
+          onCancel={handleCancel}
+        />
+      )}
     </div>
   );
 }
