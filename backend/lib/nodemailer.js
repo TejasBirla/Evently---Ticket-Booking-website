bd5371a3c970fbcb878f57b03e2db14ec51b93da
Tejas Birla <tejasbirla3@gmail.com>
2025-10-24 14:17:49 +0530

    Transporter updated.

--- backend/lib/nodemailer.js
+++ backend/lib/nodemailer.js
@@ -1,25 +1,10 @@
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USERNAME,
-//     pass: process.env.EMAIL_PASSWORD,
-//   },
-// });
-
-// export default transporter;
-
-
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
